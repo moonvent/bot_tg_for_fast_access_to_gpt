41f6debdb6e526b0aa551b65bcdfb92c796b898b
moonvent <terraboneystone@proton.me>
2023-03-27 16:44:40 +0200

    1) Fix error from openai

--- services/openai/util.py
+++ services/openai/util.py
@@ -4,6 +4,7 @@ import openai
 from services.config import OPEN_AI_MODEL
 from services.database.models.conversations import add_to_body_new_response, create_conversation, get_previous_conversation_body
 from services.database.models.sessions import add_current_conversation_to_session
+from services.logs import log
 
 
 openai.api_key = os.environ['OPENAI_TOKEN']
@@ -11,28 +12,33 @@ openai.api_key = os.environ['OPENAI_TOKEN']
 
 def send_question(text: str,
                   user_id: int,
-                  conversation_id: str = None):
+                  conversation_id: str = None) -> str:
 
     previous_body = get_previous_conversation_body(conversation_id=conversation_id)
 
     new_body = previous_body + [{"role": "user", "content": text}]
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=new_body,
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=new_body,
+            max_tokens=4096
+        )
+    except Exception as e:
+        log.exception("Error in answer generation")
+        return ERROR_IN_ANSWER_GENERATION
 
-    # add validate on response good
-    if not conversation_id:
-        conversation_id = response.id
-        create_conversation(user_id=user_id,
-                            title=text,
-                            conversation_id=conversation_id)
-        add_current_conversation_to_session(user_id=user_id,
-                                            conversation_id=conversation_id)
+    else:
+        if not conversation_id:
+            conversation_id = response.id
+            create_conversation(user_id=user_id,
+                                title=text,
+                                conversation_id=conversation_id)
+            add_current_conversation_to_session(user_id=user_id,
+                                                conversation_id=conversation_id)
 
-    add_to_body_new_response(conversation_id=conversation_id,
-                             body=new_body + [{'role': response.choices[0].message.role,
-                                               'content': response.choices[0].message.content}])
+        add_to_body_new_response(conversation_id=conversation_id,
+                                 body=new_body + [{'role': response.choices[0].message.role,
+                                                   'content': response.choices[0].message.content}])
 
-    return response.choices[0].message.content 
+        return response.choices[0].message.content 
